5e183f2c2aff125bf365d24494afff9ddd1b7635
Marcos V S Mota <marcosvinimota62@gmail.com>
2022-10-10 12:49:53 -0300

    Conditionals

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 65
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60) { // condição simples
+    console.log("voce ultrapassou a velocidade permitida. Multado!")
+}
+console.log("Diriga com cuidado!")
