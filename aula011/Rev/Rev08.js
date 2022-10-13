ce343b0e6c1b44466c3f791f23d7608899e8c60b
Marcos V S Mota <marcosvinimota62@gmail.com>
2022-10-13 15:02:47 -0300

    tests

---
+++ aula011/Rev/Rev08.js
@@ -0,0 +1,8 @@
+var fast = 80
+console.log(`A velocidade do seu carro foi ${fast} km/h`)
+
+if (fast > 90) {
+    console.log(`Você foi Multado! Ultrapassou a velocidade permitida.`)
+} else {
+    console.log(`Dirija com cuidado!`)
+}
---
+++ aula011/Rev/Rev09.js
@@ -0,0 +1,10 @@
+var país = 'Portugal'
+
+console.log(`Vivendo em ${país}.`)
+
+if (país == 'brasil'){
+    console.log
+} else {
+    console.log(`Você é estrangeiro!`)
+}
+
