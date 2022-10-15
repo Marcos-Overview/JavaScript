7529c31e045a37998ea3b8a8ea48bd59a0da9b28
Marcos V S Mota <marcosvinimota62@gmail.com>
2022-10-15 09:49:35 -0300

    While

---
+++ aula13/ambiente.js
@@ -0,0 +1,33 @@
+var c = 0
+var b = c + 1
+
+do  {
+    console.log(`Step ${c} fatia ${b}`)
+    c++ // c = c + 1
+    b = c + 1
+} while (c <= 6)
+
+
+
+
+/*
+var c = 0
+var b = c + 1
+
+while (c <= 6) {
+    console.log(`Step ${c} comi ${b}`)
+    c++ // c = c + 1
+    b = c + 1
+}
+*/
+
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?') */
+
