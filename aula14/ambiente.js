4a30abf5eb5f19944936ea17ad9c7b0af3b69df6
Marcos V S Mota <marcosvinimota62@gmail.com>
2022-10-15 10:08:36 -0300

    for

---
+++ aula14/ambiente.js
@@ -0,0 +1,15 @@
+console.log(`Vai Começar`)
+ for ( var c = 1 ; c <= 10 ; c++){
+    console.log(c)
+ }
+ console.log(`Fim`)
+
+
+
+
+ /*var c = 1
+ while (c <= 10) {
+    console.log(c)
+    c++
+ }
+*/
